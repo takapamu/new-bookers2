90fcc1df538f283166d4b535d658c654deb0bbfe
“takapamu“ <takapamukyary0129@gmail.com>
2021-05-04 15:14:32 +0000

    [フォロー・フォロワー機能]

--- bookers2-debug/app/controllers/books_controller.rb
+++ bookers2-debug/app/controllers/books_controller.rb
@@ -3,33 +3,32 @@ class BooksController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @book = Book.find(params[:id])
-    @book_comment =BookComment.new
-    @books = Book.new
-    @user = current_user
+      @book = Book.find(params[:id])
+      @book_comment =BookComment.new
+      @books = Book.new
+      @user = current_user
   end
 
   def index
-    @user = current_user
-    @books = Book.all
-    @book = Book.new
-    
+      @user = current_user
+      @books = Book.all
+      @book = Book.new
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
+       @book = Book.new(book_params)
+       @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+       redirect_to book_path(@book), notice: "You have created book successfully."
     else
-      @books = Book.all
-      render 'index'
+        @books = Book.all
+        render 'index'
     end
   end
 
   def edit
-    @book = Book.find(params[:id])
-  if @book.user == current_user
+      @book = Book.find(params[:id])
+  if  @book.user == current_user
        render "edit"
   else
        redirect_to books_path
@@ -40,24 +39,24 @@ class BooksController < ApplicationController
 
 
   def update
-    @book = Book.find(params[:id])
+       @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render "edit"
+       render "edit"
     end
   end
 
   def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to books_path
+      @book = Book.find(params[:id])
+      @book.destroy
+      redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title,:body)
+       params.require(:book).permit(:title,:body)
   end
   
   
--- bookers2-debug/app/controllers/users_controller.rb
+++ bookers2-debug/app/controllers/users_controller.rb
@@ -3,59 +3,59 @@ class UsersController < ApplicationController
    before_action :authenticate_user!
 
   def show
-    @user = User.find(params[:id])
-    @books = @user.books
-    @book = Book.new
+      @user = User.find(params[:id])
+      @books = @user.books
+      @book = Book.new
   end
 
   def index
-    @users = User.all
-    @book = Book.new
+      @users = User.all
+      @book = Book.new
   
   end
 
   def edit
-    @user = User.find(params[:id])
-  if @user == current_user
-     render "edit"
+      @user = User.find(params[:id])
+  if  @user == current_user
+      render "edit"
   else
-     redirect_to user_path(current_user)
+      redirect_to user_path(current_user)
   end
   
   end
 
   def update
-    @user = User.find(params[:id])
+       @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path(current_user), notice: "You have updated user successfully."
+       redirect_to user_path(current_user), notice: "You have updated user successfully."
     else
-      render "edit"
+       render "edit"
     end
   end
   
   def followings
-    @user = User.find(params[:id])
-    @followings = @user.following_users
+      @user = User.find(params[:id])
+      @followings = @user.following_users
   end
 
   def followers
-    @user = User.find(params[:id])
-    @followers = @user.follower_users
+      @user = User.find(params[:id])
+      @followers = @user.follower_users
   end
 
   private
   def user_params
-    params.require(:user).permit(:name,:profile_image,:introduction)
+      params.require(:user).permit(:name,:profile_image,:introduction)
   end
   
    def set_user
-    @user = User.find(params[:id])
+      @user = User.find(params[:id])
    end
 
   def ensure_correct_user
-    @user = User.find(params[:id])
-    unless @user == current_user
+      @user = User.find(params[:id])
+   unless @user == current_user
       redirect_to user_path(current_user)
-    end
+   end
   end
 end
