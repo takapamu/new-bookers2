03b9a052b1d717f036d967086f74371606d36cd0
takapamui <takapamukyary0129@gmail.com>
2021-05-07 14:55:02 +0000

    [ajax]

--- bookers2-debug/app/controllers/application_controller.rb
+++ bookers2-debug/app/controllers/application_controller.rb
@@ -5,6 +5,18 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
      user_path(resource)
   end
+  
+  def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [
+    :email,
+    :name,
+    :postcode,
+    :prefecture_name,
+    :address_city,
+    :address_street,
+    :address_building
+  ])
+  end
 
   
   protected
